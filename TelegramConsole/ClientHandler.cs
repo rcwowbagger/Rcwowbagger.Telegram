a679ad62a5677c5221b9b52f97c43e75d916c8a2
Richard Cupples <rcupples@outlook.com>
2021-11-10 22:39:22 +0100

    Add project files.

---
+++ TelegramConsole/ClientHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramConsole
+{
+    public class ClientHandler
+    {
+        private string _token;
+        private TelegramBotClient _client;
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        public ClientHandler(string token)
+        {
+            _token = token;
+            _client = new TelegramBotClient(token);
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            //_client = new TelegramBotClient("2141015782:AAFUH7apC-kuG2E1EmJR9rGAX09OmMzLyGo");
+            //var me = await _client.GetMeAsync();
+            _client.OnMessage += BotClient_OnMessage;
+        }
+
+        private void BotClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            var chatId = e.Message.Chat.Id;
+            var text = e.Message.Text.Trim();
+
+            if (text == "ping")
+            {
+                Respond(chatId, "pong").GetAwaiter().GetResult();
+            }
+
+            Console.WriteLine($"Recevied {e.Message.Text}");
+
+        }
+
+        private async Task Respond(long chatId, string message)
+        {
+            await _client.SendTextMessageAsync(chatId, message);
+        }
+
+        public void Start()
+        {
+            _client.StartReceiving(cancellationToken: _cancellationTokenSource.Token);
+        }
+
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+            _client.StopReceiving();
+        }
+
+    }
+}
---
+++ TelegramConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramConsole
+{
+    class Program
+    {
+        
+        static async Task Main(string[] args)
+        {
+            var Configuration = new ConfigurationBuilder()
+                .
+
+            _client = new TelegramBotClient("2141015782:AAFUH7apC-kuG2E1EmJR9rGAX09OmMzLyGo");
+            var me = await _client.GetMeAsync();
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+            using var cts = new CancellationTokenSource();
+
+            _client.OnMessage += BotClient_OnMessage;
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            _client.StartReceiving(cancellationToken: cts.Token);
+                //new DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync),
+                //cts.Token);
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+        }
+    }
+}
